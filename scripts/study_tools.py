9ed65c17dff2843e348dd1ae388974bf55fafb7a
fkuschel <fkuschel@gmx.de>
2022-12-05 20:37:46 +0100

    init

--- scripts/study_tools.py
+++ scripts/study_tools.py
@@ -3,4 +3,11 @@ from
 
 
 def create_input_sets(df:pd.DataFrame):
+    """
+    Input:
+        - Variables to create study
+        - Numbers for each variable or range
+    Output:
+        - Valid Input for calculation (DataClass), Validity check will be done by programm
+    """
     return ...
